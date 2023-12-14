7e1df463a038fd620be06723245358817bd09053
barc <barc>
2023-12-14 08:46:25 +0200

    try to aggrageate

--- Tirgulim/TrlNum1.R
+++ Tirgulim/TrlNum1.R
@@ -1,48 +1,25 @@
 df <- read.csv('data/intro_tutorial_dataset.csv') # nolint
-head(df, 1)
 dim(df)
-str(df)
-class(df$Boxoffice)
-hist(df$IMDb.Score)
-table(df)
-ls()
 View(head(df))
 names(df)
-NumericCol <- df [c("IMDb.Votes","Title")]
-head(NumericCol)
 
-
-BO <- df$Boxoffice
-sum(is.na(BO))
-
-sum(is.na(BO))
-head(BO)
-count()
-length(unique( BO ))
 rdf <- subset(df , select = c(Title,Series.or.Movie ,Hidden.Gem.Score,IMDb.Score,Rotten.Tomatoes.Score,Boxoffice,Release.Date,Netflix.Release.Date,IMDb.Votes,Director))
-names(df)
+dim(rdf)
 names(rdf)
 View(head(rdf))
-class(df$Title)
-length(df$Title)
-hist(rdf$IMDb.Score)
-hist(rdf$Rotten.Tomatoes.Score)
-plot(rdf$IMDb.Score~rdf$Rotten.Tomatoes.Score)
-summary(rdf$IMDb.Score)
-class(df$Boxoffice)
-df$Boxoffice
-library(tibble)
-view(tibble(head(rdf)))
 
+aggregate(rdf$Boxoffice, list(rdf$Director), FUN=sum) 
+
+rdf$Boxoffice <- gsub('[$]','', (rdf$Boxoffice))
 ListOfDirector <- unique(rdf$Director)
 sum_by_Director <- aggregate(Boxoffice ~ Director , rdf , sum)
+dim(sum_by_Director)
 class(rdf$Director)
 rdf$Director <- toString(rdf$Director)
 rdf$Boxoffice <- as.numeric(rdf$Boxoffice)
 class(rdf$Boxoffice)
 rdf$Boxoffice
 sapply(rdf ,class)
-rdf$Boxoffice <- gsub("$","", as.numeric(rdf$Boxoffice))
 
 
 
