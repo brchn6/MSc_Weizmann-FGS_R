f22f0ab00699c4b3bcea6c02f63f83833e82144a
barc <barc>
2023-12-21 07:15:53 +0200

    made the first 2 condition but count do the last 2 ones in the Q1

---
+++ Exercises/Ex2/Ex2_FirstName_LastName.R
@@ -0,0 +1,95 @@
+## exercise 2 - 2024 ##
+# Read the instructions carefully and contact the TAs with any questions you may have.
+# IMPORTANT!: Don't use CHATGPT to solve the questions. Consult one of the TA instead. 
+# Please submit the assignment file according to the template Ex2_First Name_Last Name.R
+# In this exercise, use only functions you have learned in class so far in addition to these functions:
+# mathematical operators, +-*^ and %%, and logical operators, == != > < >= <=, are allowed
+# grepl()
+# floor()
+# ceilling()
+# abs()
+# as.____() functions family. for example, as.numeric/as.character...
+# strsplit()
+# length
+# sum()
+# unlist()
+# readRDS
+#### 7-BOOM, the epic version###
+# Print (use print()) a vector of the numbers starting from 1 to 99 and replace every number that satisfied 2 of the 4 conditions with the word "BOOM".
+# Conditions: divisible by 7 (e.g., 14), has the digit 7 (e.g., 17 ), the digits sum up to seven (e.g., 25) or the absolute difference between the digits is 7 (e.g., 92 and 29). 
+# save the final vector in the variable "GAME" and print it.
+NumericList <- 1:99 #making a list of number from 1 to 99
+LogisticValue <- (NumericList%%7 == 0) # asking a logistic Q and hold it under var # nolint
+NumericList[LogisticValue] <- "BOOM"
+
+LogisticValue <- (grepl("7",NumericList)) # asking a logistic Q and hold it under var # nolint
+NumericList[LogisticValue] <- "BOOM"
+
+NumericList <- 1:99 #making a list of number from 1 to 99
+df<- as.data.frame(NumericList)
+df$NumericListAsStr <- as.character(df$NumericList)
+df$NumericListAsStrSplit <- strsplit(df$NumericListAsStr,"")
+head(df)
+# df$NumericListAsStrSplit <- gsub("[c(\"]", "",paste(df$NumericListAsStrSplit))
+# df$NumericListAsStrSplit<- gsub("[)]", "",df$NumericListAsStrSplit)
+str(df[,'NumericListAsStrSplit'])
+
+# df$SumNumericListAsStrSplit <- 
+class(df$NumericListAsStrSplit)
+sum(as.numeric(df$NumericListAsStrSplit), na.pass= T)
+
+GAME = ???
+print(GAME)
+### Convert a binary number into a decimal number ### 
+# For further explanation see the video:  https://www.youtube.com/watch?v=VLflTjd3lWA
+# Write a code that converts the binary number stored inside the variable "Binary" into a decimal number.
+# Save the solution inside the variable "Decimal" and print it. 
+# Make sure your code is general enough, and can convert any binary number.
+[ENTER CODE HERE]
+Binary = 1101
+
+Decimal = ???
+print(Decimal)
+###Convert a decimal number into a binary number ### 
+# for further explanation: https://www.youtube.com/watch?v=1TxAm9931TE
+# Write a code that converts the decimal number stored inside the variable "Dec_num" into a binary number. 
+# Save the solution inside the variable "Bin_num" and print it. 
+# Make sure your code is general enough, and can convert any decimal number.
+[ENTER CODE HERE]
+Dec_num = 2024
+
+Bin_num = ???
+print(Bin_num)
+### Convert DNA string into RNA string ###
+# Convert the DNA string in the variable DNA.string to an RNA string.
+# Change A to U, T to A, G to C and C to G
+# Store the solution in a variable called RNA.string and print it. No need to worry about the reverse complement.
+[ENTER CODE HERE]
+DNA.string = "ATGATCTCTGATATTCAAAACCACTCAGATGATGAGTCAAAAAATAATAAACCTTTCTTATTGTTTATGATAAATAATAATTGA"
+
+RNA.string =???
+print(RNA.string)
+### Convert RNA string into a protein ###
+# Run the following lines of code. The code will generate a named vector you can use to convert codons into amino acids
+# Make sure your file is in the same folder as #the RDS file, or the working directory is the same as the RDS file
+# use getwd() to see where is your current working directory. https://www.youtube.com/watch?v=TDKyTT1kAMQ 
+# use setwd() to set a new working directory. https://www.youtube.com/watch?v=TDKyTT1kAMQ
+
+codon.to.aa = readRDS("codons_to_aa.Rds")
+
+# Convert the DNA string in the variable DNA.string to an amino acids string. 
+# The first codon should be ATG then ATC, TCT... 
+# Store the solution in a variable called aa.string and print it
+
+[ENTER CODE HERE]
+
+aa.string = ???
+print(aa.string)
+
+
+
+
+
+
+
+
--- Tirgulim/TrlNum2.R
+++ Tirgulim/TrlNum2.R
@@ -1,22 +1,98 @@
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+
 df <- read.csv('data/intro_tutorial_dataset.csv') # nolint
-df <- subset(df , select = c(Title,Boxoffice,Series.or.Movie ,Hidden.Gem.Score,IMDb.Score,Rotten.Tomatoes.Score,Release.Date,Netflix.Release.Date,IMDb.Votes,Director))
+df <- subset(df , select = c(Title,Boxoffice,Director,Series.or.Movie ,Hidden.Gem.Score,IMDb.Score,Rotten.Tomatoes.Score,Release.Date,Netflix.Release.Date,IMDb.Votes,Director))
 df$Boxoffice <- gsub('[$]','', (df$Boxoffice))
 df$Boxoffice <- gsub(',','', (df$Boxoffice))
 df$Boxoffice <- as.numeric(as.integer(df$Boxoffice))
 df <- df[complete.cases(df),]
+df <- df[1:3]
+
+x <- aggregate(df$Boxoffice, by = list(df$Director), FUN = sum)
+names(x) <- c("Director", "TotalBoxOffice")
+View(head(x))
+
+y <- aggregate(df$Boxoffice, by = list(df$Director), FUN = mean)
+names(y) <- c("Director", "TotalBoxOffice")
+View(head(y))
+
+y<-aggregate(df$Boxoffice, list(df$Director), FUN="ave") 
+names(y)[1] <- "Director"
+names(y)[2] <- "Boxoffice"
+View(head(y))
+
+max_value <- max(df$Boxoffice, na.rm = TRUE,)
+avg_value <- mean(df$Boxoffice, na.rm = F)
+
 View(head(df))
+View(head(max_boxoffice))
+View(head(avg_boxoffice))
+head(df)
+            
 dim(df)
 sum(is.na(df$Boxoffice))
-a<-aggregate(df$Boxoffice, list(df$Director), FUN="sum") 
+
+# Calculate the maximum box office per director
+max_boxoffice <- aggregate(Boxoffice ~ Director, data = df, max)
+
+# Calculate the average box office per director
+avg_boxoffice <- aggregate(Boxoffice ~ Director, data = df, mean)
+
+library(ggplot2)
+
+# Assuming your data frame df has the columns Director, MaxBoxOffice, AvgBoxOffice, SumBoxOffice, Genre
+
+# Create the plot
+plot <- ggplot(df, aes(x = max_boxoffice, y = avg_boxoffice, )) +
+  geom_point(alpha = 0.7) + # Set transparency to see overlapping points
+  scale_size(range = c(2, 12)) + # Adjust the size range for sum of box office
+  theme_minimal() + # Minimal theme
+  theme(legend.position = "right") + # Position the legend on the right
+  labs(title = "Director's Box Office Earnings Comparison",
+       x = "Maximum Box Office Earnings",
+       y = "Average Box Office Earnings",
+       size = "Total Box Office Earnings",
+       color = "Common Genre") +
+  geom_text(aes(label = Director), check_overlap = TRUE, vjust = 1.5, hjust = 1.5) # Add director names
+
+# Print the plot
+print(plot)
+
+
+
+
+
+
 View((a))
-scatter.smooth(a)
-text()
+x <-
+ggplot(a, aes(x=Group.1, y=x)) + 
+    geom_point()
 
+names(data)
 
+# Libraries
+library(ggplot2)
+library(dplyr)
 
+data <- df
+# Most basic bubble plot
+ggplot(data, aes(x=Group.1, y=x, size = pop)) +
+    geom_point(alpha=0.7)
 
+max(a[x])
 
-
+names(data)
+# install.packages("tidyverse")
+# install.packages("dplyr")
+# install.packages("reshape")
+library(dplyr)
+library(tidyverse)
+library(reshape)
+data <- data %>% group_by(Director) %>% summarise(max_area= max(Boxoffice), min_area=min(Boxoffice))
+data <- data[1:90,]
+data
 let <- letters
 let
 num <- 1:26 
