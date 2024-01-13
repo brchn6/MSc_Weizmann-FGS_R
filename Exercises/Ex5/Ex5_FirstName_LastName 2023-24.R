9df5226365a5174de93a1f869a5701cfeab29033
barc <barc>
2024-01-13 12:47:24 +0200

    Q1 done

---
+++ Exercises/Ex5/Ex5_FirstName_LastName 2023-24.R
@@ -0,0 +1,131 @@
+# Exercise 5
+# Submit the file in the correct name format
+# Please contact us for questions
+# The data and the functions you will use in the following exercise are from the package 
+# renz. 
+
+# In this exercise you will explore enzymatic kinetics.
+# Load the data. Run this line of code:
+ONPG.df = readRDS("ONPG.rds")
+# Background about the OPNG.df, adopted from the renz pacakge:
+#In the University of Málaga, Enzymology is a second-year subject that all Biochemistry students 
+#must take. In the context of this subject, students carry out different experiments in the 
+#laboratory, using Beta-galactosidase (EC. 3.2.1.23) as an enzyme model, to illustrate the effect of
+#different variables on the rate of the enzyme-catalyzed reaction 
+#(hydrolysis of o-nitrophenyl-Beta-d-galactopyranoside, ONPG). 
+# One of these experiments consists in assessing the effect of the substrate (ONPG) concentration on 
+# the initial rate. The current dataframe shows the results obtained by eight different student groups,
+# as were presented in their reports.
+
+# Load the following function:
+MM.function = readRDS("MM.function.rds")
+
+# This function receives two-columns data and analyzed it using the non-linear least-squares fitting of the Michaelis-Menten equation.
+# For more information see: https://www.youtube.com/watch?v=7u2MkbsE_dw
+# The function takes the following arguments (adopted from the renz pacakge):
+# data:	
+# a dataframe with two columns. The first column contains the values of the independent variable (substrate concentration), and the second column contains the initial rates.
+# unit_S:	
+# concentration unit (nM by default).
+# unit_v:
+# kinetics unit (a.u by default).
+# plot:	
+# logical. If true, the data and fitted curve are plotted.
+
+# Output (adopoted from the renz package)
+#A list of two elements. The first one is a vector containing the enzyme kinetic parameters.
+# The second one is a dataframe with the original data plus the fitted value of v.
+
+#load the renz library
+# install.packages("renz")
+library(renz, help, pos = 2, lib.loc = NULL)
+
+
+# Exapmle:
+# Column 1: contains the substrate concentrations
+# Column 2: the observed kinetics of group 1
+Analysis.results = MM.function(ONPG.df[,1:2])
+# In "parameters" you can find the calculate Km and Vm (Vmax)
+Analysis.results$parameters
+# In "data" you can find a summary of the experiment, substrate concentrations, observed values and fitted values
+Analysis.results$data
+
+
+#######################
+# Q1 #
+# Look at the data. See that you understand it.
+# Explore if there is any different between the groups.
+# Print the observed Km and Vmax parameters for each group
+OPNG_VectorOfConcentrations <- ONPG.df[,1]
+for (index in 1:length(OPNG_DF_OfGroups)){
+  data <- as.data.frame(cbind(OPNG_VectorOfConcentrations , OPNG_DF_OfGroups[index]))
+  GroupName <- names(OPNG_DF_OfGroups[index])
+  Analysis.results = MM.function(data)
+  # In "parameters" you can find the calculate Km and Vm (Vmax)
+  print(paste("Group name:" ,GroupName))
+  print(Analysis.results$parameters)
+  # In "data" you can find a summary of the experiment, substrate concentrations, observed values and fitted values
+  Analysis.results$data
+}
+
+# Naturally there are differences in the observed Km and Vmax. 
+# Please plot the mean Km and the mean Vmax of all groups as a bar plot using ggplot.
+# Add a text above the bars to indicate the value
+# Hint: use geom_text, with nudge_y = 1.5
+# Use Ex 5 2023-24 plot 1.pdf to see the expected result
+
+ENTER CODE HERE
+
+# Q2 #
+# Now days it is a common request to see  points that represent each observation.
+# Add them to the plot (using ggplot).
+# You will need to another layer of points using geom_point. 
+# In geom text use nudge_y = 2
+# look at Ex 5 2023-24 plot 2 to see the expected plot
+
+ENTER CODE HERE
+
+
+# Q3 #
+# As you can see there is a problem, many points overlay each others.
+# Add to the ggplot object the function scale_y_continuous(trans = "log2"), to change the scale of the 
+# y-axis and to see better the data. 
+# look at Ex 5 2023-24 plot 3 to see the expected plot
+
+ENTER CODE HERE
+
+
+# Q4 #
+# A different method will be to use geom_dotplot.
+# use it instead of geom_point to nicely show the data
+# geom_dotplot is a combination of points and histogram. you will need to add 
+# stackdir = "center",binaxis = "y",dotsize = 0.5 inside geom_dotplot(). 
+# use nudge_y = 3 in geom_text
+# look at Ex 5 2023-24 plot 4 to see the expected plot
+
+ENTER CODE HERE
+
+
+# Bonus: add error bars to the plot
+# Hint use geom_errorbar()
+# You will need to add ymin and ymax values (iniside the aes()). ymin is the average Km or Vmax - SD of the Km or Vmax. 
+# ymax is almost the same: The average Km or Vmax + SD of the Km or Vmax
+# For more details: http://www.sthda.com/english/wiki/ggplot2-error-bars-quick-start-guide-r-software-and-data-visualization
+# look at Ex 5 2023-24 plot 5 to see the expected plot
+
+ENTER CODE HERE
+
+# Q5 #
+# Plot the data for each group.
+# Show the dots (geom_point) of the observed values and add the fitted line (fitted_v) using geom_line() 
+# Colors the points and lines based on the different groups.
+# Add to the ggplot object scale_y_continuous(trans = "log2")
+# look at Ex 5 2023-24 plot 6 to see the expected plot
+
+ENTER CODE HERE
+
+
+# Use facet_wrap() to show the results of each group in a subpanel 
+# Look at Ex 5 2023-24 plot 7 to see the expected plot
+
+ENTER CODE HERE
---
+++ Tirgulim/tirgul5/R tutorial 5 - 2023-24.R.txt.R
@@ -0,0 +1,199 @@
+# Load data
+data("iris")
+# Basic plotting
+boxplot(Sepal.Length~Species,iris,col = c(1:3))
+hist(iris$Sepal.Width,col = "red")
+plot(iris$Sepal.Length,iris$Sepal.Width, col = iris$Species)
+
+iris
+###
+############ ggplot ############
+# ggplot is a one stop shop that uses tables to plot graphs of different formats, and to combine between them
+# working with ggplot is extremely simple. 
+# We will start by plotting a dot plot. 
+# Calling ggplot() initiates the the function. Always start with it.  
+library(ggplot2, help, pos = 2, lib.loc = NULL)
+ggplot()+
+  # Calling geom_point() tells ggplot we want our graph to be in the shape of a dot plot. 
+  # there are many type of geom_(s), each generates a different type of graph: histogram, box plot, lines...
+  # data = iris, tells the geom_ to use iris as the source of the data. 
+  # mapping = aes(x = Sepal.Length, y = Sepal.Width), tells ggplot which columns to use for the plotting
+  # aes() may looks confusing, in general, aes() is used whenever you want to use the data from table to set xy coordinates/change color/shape based on a factor in the data 
+  # that's it
+  geom_point(data = iris, mapping = aes(x = Sepal.Length, y = Sepal.Width))
+## Adding colors based on groups ##
+ggplot()+ geom_point(data = iris,mapping = aes(x = Sepal.Length, y = Sepal.Width, col = Species))
+A <- ggplot()+ geom_histogram(data =iris , mapping = aes(x = Sepal.Length))
+B <- ggplot()+ geom_bar(data =iris , mapping = aes(x = Sepal.Length))
+install.packages("gridExtra")
+library("gridExtra")
+install.packages("cowplot")
+library("cowplot")
+plot_grid(A, B, labels=c("A", "B"), ncol = 1, nrow = 2)
+
+## Changing shapes based on groups ##
+A <- ggplot()+
+  # aes() may looks confusing, but think in general, aes() is used whenever you want to use the data the table to plot/color/shape 
+  geom_point(data = iris,
+             mapping = aes(x = Sepal.Length, y = Sepal.Width, shape = Species, col = Species)
+             ,size = 2)
+
+## Adding colors not based on groups ##
+ggplot()+
+  # aes() may looks confusing, but in general, aes() is used whenever you want to use the data in the table to define xy coordinates/color based on a variable/change shape based on a variable 
+  geom_point(data = iris,
+             mapping = aes(x = Sepal.Length, y = Sepal.Width),
+             col = "red")
+## Adding another layer ##
+ggplot()+
+  geom_point(data = iris,
+             mapping = aes(x = Sepal.Length, y = Sepal.Width))+
+  geom_text(data = iris,
+            mapping = aes(x = Sepal.Length, y = Sepal.Width,label = Species))
+
+ggplot()+
+  geom_point(data = iris,
+             mapping = aes(x = Sepal.Length, y = Sepal.Width))+
+  # label plot labels instead of dots or bars. 
+  # nudge_x/nudge_y shifts the position of the label in the x or y direction
+  geom_text(data = iris,
+            mapping = aes(x = Sepal.Length, y = Sepal.Width,label = Species),
+            size = 2, 
+            nudge_y = 0.1)
+
+ggplot()+
+  geom_point(data = iris,
+             mapping = aes(x = Sepal.Length, y = Sepal.Width))+
+  geom_text(data = iris,
+            mapping = aes(x = Sepal.Length, y = Sepal.Width*1.01, label = Species),
+            size = 2)
+
+# After lunching ggplot by ggplot(), each layer is almost an independent unit. Each layer can accept different data.
+# In the absence of data, or missing values in the aes() the layer looks up to the ggplot() function. 
+# ggplot() can serve as a source of "default" data. 
+# But be carful, if you define parameters inside ggplot() they can affect all the layers downstream
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width,label = Species))+
+  geom_point()+
+  geom_text(size = 2, 
+            nudge_y = 0.1)
+
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width,label = Species))+
+  geom_point(col = "red")+
+  geom_text(size = 2, 
+            nudge_y = 0.1)
+
+# We can split the data into subgroups and display each group in a different subpanel
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width))+
+  geom_point(shape = 21)+
+  geom_smooth(method = "lm")+
+  # facet_wrap() breaks the data into one dimensional sub-panels
+  # facet_grid(x~y),breaks the data into two dimensions sub-panels
+  facet_wrap(~Species)+
+  # theme_bw/theme_classic change the appearance of the graph
+  theme_dark()+
+  #theme() controls the look of the plot, size and shape of the axis for example
+  theme(aspect.ratio = 1,
+        panel.grid = element_blank(),
+        axis.text = element_text(face = "bold",size = 10))
+
+# Another exapmle 
+ggplot()+
+  geom_histogram(data = iris,
+                 mapping = aes(x = Sepal.Length ),fill = "red", alpha = 0.5,col = "black")+
+  geom_freqpoly(data = iris,
+                mapping = aes(x = Sepal.Length ))+
+  geom_histogram(data = iris,
+                 mapping = aes(x = Sepal.Width ),fill = "blue", alpha = 0.5,col = "black")+
+  geom_freqpoly(data = iris,
+                mapping = aes(x = Sepal.Width ))+
+  facet_wrap(~Species)+
+  theme_bw()+
+  theme(aspect.ratio = 1,
+        panel.grid = element_blank(),
+        axis.text = element_text(face = "bold",size = 10))+
+  xlab("")
+
+############ loops how to use them ############
+avg = c()
+for (i in 1:ncol(iris)) {
+  print(paste("i has the value of",i))
+  column.avg = mean(iris[,i])
+  avg[i] = column.avg
+  #avg = c(avg,column.avg), is another option
+}
+
+for (i in 1:ncol(iris)) {
+  print(paste("i has the value of",i))
+  print(class(iris[,i]))
+}
+
+for (i in 1:ncol(iris[,-5])) {
+  print(paste("i has the value of ",i))
+  print(paste("The avg of column" , names(iris)[i] , "was calculated"))
+  avg[i] = column.avg
+}
+
+# loop within a loop
+
+prod.values = c()
+
+for (COLUMNS in 1:9) {
+  print(paste("This is column", COLUMNS))
+  for(ROWS in 1:9){
+  print(paste("This is row", COLUMNS))
+  }
+}
+
+for (COLUMNS in 1:9) {
+  print(paste("This is column", COLUMNS))
+  for(ROWS in 1:9){
+    print(paste("This is row", COLUMNS))
+    product = COLUMNS*ROWS
+    prod.values = c(prod.values,product)
+  }
+}
+matrix(data = prod.values,ncol = 9,nrow = 9)
+
+
+# Finding the mean value of each parameter for each type of iris
+for (iris.types in unique(iris$Species)){
+  print(iris.types)
+  for (iris.paprmeter in names(iris[,1:4])) {
+    print(iris.paprmeter)
+    mean.value = mean(iris[iris$Species==iris.types,iris.paprmeter])
+    print(paste("The mean value of the", iris.paprmeter, "of", iris.types, "is", mean.value))
+  }
+}
+###### apply #####
+
+apply(iris[,-5], MARGIN = 2, FUN = mean)
+
+##### lapply #####
+# lapply() goes over the elements in list one by one, and preforms a function on those elements. Because lapply() works with
+# lists, its output is also a list
+# IMPORTANT!!!  a data frame is also a list, but in a nicer format. Each column is an element in the list 
+lapply(iris[,-5], function(x){x/sd(x)})
+scaled.data_list = lapply(iris[,-5], function(x){x/sd(x)})
+scaled.data_table = as.data.frame(do.call(cbind, scaled.data_list))
+scaled.data_table$Species = iris$Species
+
+
+##### sapply #####
+# Try to simplify the product of lapply
+# IMPORTANT!!! the results of the simplification might be unpredictable. 
+sapply(iris[,-5], function(x){x/sd(x)})
+
+
+##### tapply #####
+
+tapply(iris$Sepal.Length, iris$Species, mean)
+
+for (iris.paprmeter in names(iris[,1:4])) {
+  print(iris.paprmeter)
+  print(tapply(iris[[iris.paprmeter]], iris$Species, mean))
+}
+
+
+
+
+
